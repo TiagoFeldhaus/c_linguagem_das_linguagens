f46383f9dc5c7e2d36d384aed782ca9d1781e56d
Tiagão <tiago.feldhaus.siqueira@escola.pr.gov.br>
2025-04-25 07:58:09 -0300

    jogo

---
+++ adivinhacao.c
@@ -0,0 +1,13 @@
+int main () {
+    printf("******************************************************\n");
+    printf("*                      Bem Vindo                     *\n");
+    printf("******************************************************\n");
+
+    int numeroescondido = 7325642;
+
+    int chute;
+
+    printf("Qual o seu chute?");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", &chute)
+}
