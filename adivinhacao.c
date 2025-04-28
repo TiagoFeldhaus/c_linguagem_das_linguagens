ea7587a424a01690fe5c227c33a53df0e9880d67
Tiagão <tiago.feldhaus.siqueira@escola.pr.gov.br>
2025-04-28 12:22:02 -0300

    mudanças

--- adivinhacao.c
+++ adivinhacao.c
@@ -10,4 +10,20 @@ int main () {
     printf("Qual o seu chute?");
     scanf("%d", &chute);
     printf("Seu chute foi %d\n", &chute)
+
+    int acertou = (chute == numeroescondido);
+
+    if(acertou){
+    printf("Acertou!\n");
+    printf("Jogue de novo!\n")
+    }
+    else {
+
+        if(chute > numeroescondido){
+            printf("O número é menor\n")
+        }
+        if(chute < numeroescondido){
+            printf("O número é maior\n")
+        }
+    }
 }
